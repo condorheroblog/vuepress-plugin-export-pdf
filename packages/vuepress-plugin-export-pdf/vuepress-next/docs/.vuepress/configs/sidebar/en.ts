b081bc6cfdcac4a2ccf9edcf24ed374a66a92053
CondorHero <love2xinwei@gmail.com>
2022-07-18 15:18:29 +0800

    feat: add vuepress-next project

---
+++ packages/vuepress-plugin-export-pdf/vuepress-next/docs/.vuepress/configs/sidebar/en.ts
@@ -0,0 +1,23 @@
+import type { SidebarConfig } from 'vuepress'
+
+export const sidebarEn: SidebarConfig = {
+  '/guide/': [
+    {
+      text: 'Guide',
+      children: [
+        '/guide/README.md',
+        '/guide/assets.md',
+        '/guide/bundler.md',
+      ],
+    },
+  ],
+  '/advanced/': [
+    {
+      text: 'Advanced',
+      children: [
+        '/advanced/plugin.md',
+        '/advanced/theme.md',
+      ],
+    }
+  ]
+}
---
+++ packages/vuepress-plugin-export-pdf/vuepress-next/docs/.vuepress/configs/sidebar/zh.ts
@@ -0,0 +1,24 @@
+import type { SidebarConfig } from 'vuepress'
+
+export const sidebarZh: SidebarConfig = {
+  '/zh/guide/': [
+    {
+      text: '指南',
+      children: [
+        '/zh/guide/README.md',
+        '/zh/guide/assets.md',
+        '/zh/guide/bundler.md',
+      ],
+    },
+  ],
+  '/zh/advanced/': [
+    {
+      text: '深入',
+      children: [
+        '/zh/advanced/architecture.md',
+        '/zh/advanced/plugin.md',
+        '/zh/advanced/theme.md',
+      ],
+    },
+  ],
+}
