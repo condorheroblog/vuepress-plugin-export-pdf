cfe91d0c24791eb09c085d43bd57a4a39f8ebd0d
CondorHero <love2xinwei@gmail.com>
2022-07-28 11:41:50 +0800

    chore: remove @vuepress/utils ora TS error comment

--- packages/vuepress-plugin-export-pdf-v2/src/systemInfo.ts
+++ packages/vuepress-plugin-export-pdf-v2/src/systemInfo.ts
@@ -5,10 +5,6 @@ import envinfo from "envinfo";
  * Display environment information.
  */
 export const systemInfo = async(): Promise<void> => {
-  // TODO: report @vuepress/utils
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-expect-error
-  // eslint-enable @typescript-eslint/ban-ts-comment
   const spinner = ora();
   spinner.start(chalk.bold("\nCollecting Environment Info:"));
 
