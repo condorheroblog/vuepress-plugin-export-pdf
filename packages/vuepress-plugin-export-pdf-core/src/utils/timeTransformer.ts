06c2686cf9812331859f6fe410505cbc3136839f
CondorHero <love2xinwei@gmail.com>
2023-06-26 14:22:33 +0800

    fix(core): use [zh-CN](https://www.techonthenet.com/js/language_tags.php) language tags option in timeTransformer function

--- packages/vuepress-plugin-export-pdf-core/src/utils/timeTransformer.ts
+++ packages/vuepress-plugin-export-pdf-core/src/utils/timeTransformer.ts
@@ -1,3 +1,13 @@
+const options: Intl.DateTimeFormatOptions = {
+	year: "numeric",
+	month: "2-digit",
+	day: "2-digit",
+	hour: "2-digit",
+	minute: "2-digit",
+	second: "2-digit",
+	hour12: false,
+};
+
 /**
  * get current time in format: YYYY-MM-DD-HH-mm-ss
  * @param timestamp - Timestamp
@@ -5,6 +15,6 @@
  * @param dateOptions - Date options
  * @returns - Date string
  */
-export function timeTransformer(timestamp = new Date(), lang = "cn", dateOptions = {}) {
+export function timeTransformer(timestamp = new Date(), lang = "zh-CN", dateOptions = options) {
 	return new Date(timestamp).toLocaleString(lang, dateOptions).replaceAll(/(\/|\:|)/g, "").replace(/\s/, "-");
 }
